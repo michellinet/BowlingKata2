11bc20961d0b1d9b330e7759d950a92ac1190dfc
michellinet <michellinet@procore.com>
2017-06-13 18:15:48 -0700

    Setting up.

---
+++ Bowling2/Scorecard.swift
@@ -0,0 +1,16 @@
+//
+//  Scorecard.swift
+//  Bowling2
+//
+//  Created by Michelline Tran on 6/13/17.
+//  Copyright © 2017 Michelline Tran. All rights reserved.
+//
+
+import Foundation
+
+
+class Scorecard {
+    
+    
+    
+}
